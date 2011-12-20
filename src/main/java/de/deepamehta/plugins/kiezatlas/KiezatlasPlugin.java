3ca8911535ca8845f86a4e21104212250611df47
Jörg Richter <jri@deepamehta.de>
2011-12-20 04:00:53 +0100

    Geo object form: process website-specific facets.

--- src/main/java/de/deepamehta/plugins/kiezatlas/KiezatlasPlugin.java
+++ src/main/java/de/deepamehta/plugins/kiezatlas/KiezatlasPlugin.java
@@ -1,6 +1,9 @@
 package de.deepamehta.plugins.kiezatlas;
 
+import de.deepamehta.plugins.facets.service.FacetsService;
+
 import de.deepamehta.core.Association;
+import de.deepamehta.core.AssociationDefinition;
 import de.deepamehta.core.RelatedTopic;
 import de.deepamehta.core.ResultSet;
 import de.deepamehta.core.Topic;
@@ -9,7 +12,10 @@ import de.deepamehta.core.model.AssociationRoleModel;
 import de.deepamehta.core.model.CompositeValue;
 import de.deepamehta.core.model.TopicModel;
 import de.deepamehta.core.model.TopicRoleModel;
+import de.deepamehta.core.service.ClientState;
+import de.deepamehta.core.service.Directives;
 import de.deepamehta.core.service.Plugin;
+import de.deepamehta.core.service.PluginService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -45,9 +51,76 @@ public class KiezatlasPlugin extends Plugin {
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
+    private FacetsService facetsService;
+
     private Logger logger = Logger.getLogger(getClass().getName());
 
     // -------------------------------------------------------------------------------------------------- Public Methods
+
+
+
+
+    // **************************************************
+    // *** Core Hooks (called from DeepaMehta 4 Core) ***
+    // **************************************************
+
+
+
+    @Override
+    public void serviceArrived(PluginService service) {
+        logger.info("########## Service arrived: " + service);
+        if (service instanceof FacetsService) {
+            facetsService = (FacetsService) service;
+        }
+    }
+
+    @Override
+    public void serviceGone(PluginService service) {
+        logger.info("########## Service gone: " + service);
+        if (service == facetsService) {
+            facetsService = null;
+        }
+    }
+
+    // ---
+
+    @Override
+    public void postUpdateHook(Topic topic, TopicModel newModel, TopicModel oldModel, ClientState clientState,
+                                                                                      Directives directives) {
+        if (topic.getTypeUri().equals("dm4.kiezatlas.geo_object")) {
+            long topicmapId = clientState.getLong("dm4_topicmap_id");
+            logger.info("### Updating geo object facets (topicmapId=" + topicmapId + ")");
+            //
+            if (!isGeomap(topicmapId)) {
+                logger.info("Updating geo object facets ABORTED -- topicmap " + topicmapId + " is not a geomap");
+                return;
+            }
+            //
+            Topic website = getWebsite(topicmapId);
+            if (website == null) {
+                logger.info("Updating geo object facets ABORTED -- geomap " + topicmapId +
+                    " is not part of a Kiezatlas website");
+                return;
+            }
+            //
+            for (Topic facetType : getFacetTypes(website.getId())) {
+                String facetTypeUri = facetType.getUri();
+                String assocDefUri = getAssocDef(facetTypeUri).getUri();
+                TopicModel facet = newModel.getCompositeValue().getTopic(assocDefUri);
+                logger.info("### Adding facet to topic " + topic.getId() + " (facetTypeUri=" + facetTypeUri +
+                    ", facet=" + facet + ")");
+                facetsService.addFacet(topic, facetTypeUri, facet, clientState, directives);
+            }
+        }
+    }
+
+
+
+    // **********************
+    // *** Plugin Service ***
+    // **********************
+
+
 
     @GET
     @Path("/{url}")
@@ -81,5 +154,19 @@ public class KiezatlasPlugin extends Plugin {
         } catch (Exception e) {
             throw new RuntimeException("Finding the website's facet types failed (websiteId=" + websiteId + ")");
         }
+    }
+
+    // ------------------------------------------------------------------------------------------------- Private Methods
+
+    private boolean isGeomap(long topicmapId) {
+        Topic topicmap = dms.getTopic(topicmapId, true, null);
+        String rendererUri = topicmap.getCompositeValue().getString("dm4.topicmaps.topicmap_renderer_uri");
+        return rendererUri.equals("dm4.geomaps.geomap_renderer");
+    }
+
+    // ### FIXME: there is a copy in FacetsPlugin.java
+    private AssociationDefinition getAssocDef(String facetTypeUri) {
+        // Note: a facet type has exactly *one* association definition
+        return dms.getTopicType(facetTypeUri, null).getAssocDefs().values().iterator().next();
     }
 }
